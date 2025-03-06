22e75178dacf2c934389fe93a939352048a47bbd
Lisa <54662679+abbliseng@users.noreply.github.com>
2025-03-06 12:58:52 +0100

    setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Main from './Views/Main.tsx';
+
+
+function App() {
+  return (
+    <Main />
+  );
+}
+
+export default App;
---
+++ src/Views/Main.tsx
@@ -0,0 +1,17 @@
+import logo from "../Assets/ProjectDive.svg";
+import "../App.css";
+
+function Main() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          basic test thingy :P
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Main;
