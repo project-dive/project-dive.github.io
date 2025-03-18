6590531af2f4d029ad2894c9b7fb713e0e1f4266
Meya Wikner <meya.wikner@gmail.com>
2025-03-18 12:58:04 +0100

    Ladning page (#16)
    * Fontdisrepencies löst
    
    * Moved theming to utils file
    
    ---------
    
    Co-authored-by: meya <meya@kth.se>

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ import GuestBook from './views/GuestBookView';
 import Events from './views/EventsView';
 
 import Navbar from "./components/Navbar";
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Theme from "./utils/theme";
+import { ThemeProvider } from '@mui/material/styles';
 import './App.css';
 
 import { BrowserRouter, Route, Routes} from "react-router-dom";
@@ -62,33 +63,3 @@ function App() {
 }
 
 export default App
-
-
-const theme = createTheme({
-  palette: {
-    mode: 'light',
-    primary: {
-      main: '#6d2c86',
-    },
-    secondary: {
-      main: '#FABF36',
-    },
-    background: {
-      default: '#FFF5C2',
-      paper: '#ffffff',
-    },
-    error: {
-      main: '#FF521B',
-    },
-    info: {
-      main: '#7C9EB2',
-    },
-  },
-  typography: {
-    fontFamily: 'Figtree',
-    fontWeightRegular: 500,
-    fontWeightMedium: 600,
-    fontWeightBold: 800,
-    fontWeightLight: 400,
-  },
-});
