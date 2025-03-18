252dbb976da8b627f2db8b5cae5bf03ad4b2fd9b
Meya Wikner <meya.wikner@gmail.com>
2025-03-18 16:42:52 +0100

    Events view (#26)
    * Började arbeta på eventsidan
    
    * Started work on events view
    
    ---------
    
    Co-authored-by: jakob <jakob.pettersson10@gmail.com>
    Co-authored-by: meya <meya@kth.se>

--- src/views/EventsView.tsx
+++ src/views/EventsView.tsx
@@ -1,8 +1,36 @@
 import React from "react";
+import EventItem from "../components/eventsViewComponents/EventItem";
+import eventsList from "../../data/events.json";
 
 export default function Events() {
-  return <div>
-      <h1>Välkommen till Project Dive!</h1>
-      <h2>Men vilka är vi?</h2>
-  </div>;
+  const sortByDate = (eventA, eventB) => {
+    return eventA.date === eventB.date ? 0 : eventA.date < eventB.date ? -1 : 1;
+  };
+
+  const events = eventsList["events"];
+  events.sort(sortByDate)
+
+  return (
+    <div>
+      <h1>Events</h1>
+      {events.map(
+        (event: {
+          name: string;
+          date: string;
+          text: string;
+          link?: string;
+          linkText?: string;
+        }) => (
+          <EventItem
+            title={event.name}
+            date={event.date}
+            text={event.text}
+            link={event.link}
+            linkText={event.linkText}
+            key={events.indexOf(event)}
+          />
+        )
+      )}
+    </div>
+  );
 }
