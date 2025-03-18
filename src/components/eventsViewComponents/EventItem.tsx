641b9271f1a9b18e272f7b2fb001830b573a0804
jakob <jakob.pettersson10@gmail.com>
2025-03-18 16:35:58 +0100

    Started work on events view

--- src/components/eventsViewComponents/EventItem.tsx
+++ src/components/eventsViewComponents/EventItem.tsx
@@ -1,17 +1,71 @@
 import * as React from "react";
+import Button from "@mui/material/Button";
 
 interface EventItemProps {
   title: string;
   date: string;
   text: string;
   link?: string;
+  linkText?: string;
 }
 
-export default function EventItem({ title, date, text, link }: EventItemProps) {
+export default function EventItem({
+  title,
+  date,
+  text,
+  link,
+  linkText,
+}: EventItemProps) {
+  const handleLinkClick = () => {
+    window.open(link);
+  };
+
   return (
-    <div>
-      {date}, {title}, {text}
-      {link && <a href={link} target="_blank">länk</a>}
+    <div style={style.container}>
+      <div style={style.titleContainer}>
+        <span style={style.date}>{date}</span>
+        <span style={style.title}> - {title}</span>
+      </div>
+      <div style={style.infoText}>{text}</div>
+      {link && (
+        <Button variant="contained" onClick={handleLinkClick}>
+          {linkText ? linkText : "placeholder text"}
+        </Button>
+      )}
     </div>
   );
 }
+
+const style = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    margin: "20px 20%",
+    padding: "30px",
+    maxWidth: "60%",
+    backgroundColor: "#F7EDBE",
+    outline: "5px solid #FAB510",
+    borderRadius: "2rem",
+
+  },
+  titleContainer: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  date: {
+    color: "#FAB510",
+    fontWeight: "bold",
+    fontSize: "x-large",
+    margin: "0 5px 0 0",
+  },
+  title: {
+    color: "#6d2c86",
+    fontWeight: "bold",
+    fontSize: "x-large",
+  },
+  infoText: {
+    textAlign: "left",
+    fontSize: "large",
+    margin: "10px 0"
+  }
+};
--- src/views/EventsView.tsx
+++ src/views/EventsView.tsx
@@ -3,7 +3,12 @@ import EventItem from "../components/eventsViewComponents/EventItem";
 import eventsList from "../../data/events.json";
 
 export default function Events() {
-  const events = eventsList["events"]
+  const sortByDate = (eventA, eventB) => {
+    return eventA.date === eventB.date ? 0 : eventA.date < eventB.date ? -1 : 1;
+  };
+
+  const events = eventsList["events"];
+  events.sort(sortByDate)
 
   return (
     <div>
@@ -14,8 +19,16 @@ export default function Events() {
           date: string;
           text: string;
           link?: string;
+          linkText?: string;
         }) => (
-          <EventItem title={event.name} date={event.date} text={event.text} link={event.link} key={events.indexOf(event)}/>
+          <EventItem
+            title={event.name}
+            date={event.date}
+            text={event.text}
+            link={event.link}
+            linkText={event.linkText}
+            key={events.indexOf(event)}
+          />
         )
       )}
     </div>
