14a19443682ff93b7212405d9452e50f13c3b365
Lisa <54662679+abbliseng@users.noreply.github.com>
2026-01-30 11:27:56 +0100

    Purge and rebirth (#39)
    * the purgeeeeee 🎅
    
    * uhh i want the readme (unpurge)
    
    * Enhance README with Shadcn and PNPM details
    
    Updated project description to include Shadcn and PNPM.
    
    * init new setup 👻
    
    * Update README to remove template details
    
    Removed description of the template and plugins.
    
    * Revise README for project setup and component usage
    
    Updated README to reflect changes in project structure and installation instructions, including new component addition process.

--- README.md
+++ README.md
@@ -2,19 +2,27 @@
 
 Välkommen till ~Datateknik~ Dive !
 
-# React + Vite
-
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend using TypeScript and enable type-aware lint rules. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
-
+# React/Vite/Tailwind/Shadcn/PNPM
+### How to run?
+Run the following command to update and install all dependencies:
+```
+pnpm i
+```
+Then start the local development server:
+```
+pnpm run dev
+```
+### Uhhhh I want a new component?
+(Quicknote, these use `npx` not `pnpm` cause it didnt work for me otherwise but if you have a way please let me know)
+```
+npx shadcn@latest add <COMPONENT>
+```
+Button example:
+```
+npx shadcn@latest add button
+```
+They will then be added to the ./src/components/ui directory in the repo. From which you are now free to import the component into whatever place you want :D
+See avalible components here: https://ui.shadcn.com/docs/components
 
 # Development log
 ### Website after Dive 25/26
--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,36 @@
-import { Box, ThemeProvider } from '@mui/material';
-import React from "react";
-import './App.css';
-
-import Home from "views/HomeView";
-// import About from "views/AboutView";
-import ChapterInfo from "views/ChapterInfoView";
-// import GuestBook from "views/GuestBookView";
-import Events from "views/EventsView";
-import FAQ from "views/FAQView";
-
-import banner from "assets/borggardencrop.jpg";
-import Footer from "components/Footer";
-import Navbar from "components/Navbar";
-import Themes from "utils/theme";
-
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
-  const routes = [
-    {
-      component: Home,
-      path: "/",
-      title: "Project Dive",
-      icon: "home",
-    },
-    {
-      component: Events,
-      path: "/events",
-      title: "Event",
-      icon: "book",
-    },
-    {
-      component: ChapterInfo,
-      path: "/chapter",
-      title: "Sektionen",
-      icon: "info",
-    },
-    // {
-    //   component: GuestBook,
-    //   path: "/guestbook",
-    //   title: "Guest Book",
-    //   icon: "book",
-    // },
-    {
-      component: FAQ,
-      path: "/faq",
-      title: "FAQ",
-      icon: "book",
-    },
-    // {
-    //   component: About,
-    //   path: "/#/about",
-    //   title: "Om oss",
-    //   icon: "info",
-    // },
-  ];
+  const [count, setCount] = useState(0)
 
   return (
-    <HashRouter>
-      <ThemeProvider theme={Themes}>  
-        <Navbar routes={routes} />
-        <Box component="div" style={style.imgBox} src={banner}>
-            <img width={"100%"} src={banner}/>
-        </Box>
-        <Routes>
-          {routes.map((route, index) => (
-            <Route
-              key={index}
-              path={route.path}
-              element={<route.component />}
-            ></Route>
-          ))}
-        </Routes>
-        <Footer/>
-      </ThemeProvider>
-    </HashRouter>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <Button onClick={() => setCount((count) => count + 1)} style={{"color": "black"}}>
+          count is {count}
+        </Button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
 export default App
-
-const style = {
-  imgBox: {
-    width: '100%',
-    maxHeight: '25vh',
-    top: '0',
-    left: '0',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    overflow: "hidden",
-    maskImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 1) 70%, rgba(0, 0, 0, 0) 95%)',
-  },
-}
